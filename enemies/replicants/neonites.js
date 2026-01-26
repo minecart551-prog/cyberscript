da1a8fa37756349fc95163c8e1537ebad576d41a
minecart551-prog <minecart551@gmail.com>
2026-01-25 17:04:19 -0800

    Update neonites.js

--- enemies/replicants/neonites.js
+++ enemies/replicants/neonites.js
@@ -5,5 +5,13 @@ function init(event){
      npc.getStats().getRanged().setStrength(4);
      npc.getStats().getRanged().setDelay(17, 17);
      npc.getStats().getRanged().setBurstDelay(1);
- 
+     var coal = npc.world.createItem("minecraft:coal", 1);
+     var beans = npc.world.createItem("minecraft:cocoa_beans", 1);
+     var milk = npc.world.createItem("farmersdelight:milk_bottle", 1);
+     var mug = npc.world.createItem("yuushya:mug", 1);
+     npc.getInventory().setDropItem(1, coal, 20);
+     npc.getInventory().setDropItem(2, beans, 20);
+     npc.getInventory().setDropItem(3, milk, 20);
+     npc.getInventory().setDropItem(4, mug, 20);
+
 }
